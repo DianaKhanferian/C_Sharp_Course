6febae573a24ecf1a194d2daf725445d0f4f0319
Diana <diana.xk@mail.ru>
2022-12-13 03:12:52 +0200

    dz2

--- 6_lesson/dz2/Program.cs
+++ 6_lesson/dz2/Program.cs
@@ -5,3 +5,25 @@
 // b1 = 2, k1 = 5, b2 = 4, k2 =
 // 9 -> (-0,5; -0,5)
 
+void Intersection(double b1, double k1, double b2, double k2)
+{
+   double x = (b2 - b1) / (k1 - k2);
+   double y = k1 * x + b1;
+   
+    // double x = (b2 - b1) / (k1 - k2);
+    // double y = k2 * x + b2;
+
+    Console.WriteLine($"Точка пересечения: X: {x}, Y: {y}");
+}
+
+Console.Write("Введите b1: ");
+double b1 = double.Parse(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = double.Parse(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = double.Parse(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = double.Parse(Console.ReadLine());
+Intersection(b1, k1, b2, k2);
+
+
