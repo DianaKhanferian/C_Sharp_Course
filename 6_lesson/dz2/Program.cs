6b09796f69a46802865b9cd28ee27160afdfccb5
Diana <diana.xk@mail.ru>
2022-12-13 13:52:21 +0200

    dz2

--- 6_lesson/dz2/Program.cs
+++ 6_lesson/dz2/Program.cs
@@ -9,9 +9,6 @@ void Intersection(double b1, double k1, double b2, double k2)
 {
    double x = (b2 - b1) / (k1 - k2);
    double y = k1 * x + b1;
-   
-    // double x = (b2 - b1) / (k1 - k2);
-    // double y = k2 * x + b2;
 
     Console.WriteLine($"Точка пересечения: X: {x}, Y: {y}");
 }
